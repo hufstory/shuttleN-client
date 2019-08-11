ed868e053c92b25e9614b6558a4f646de40df5ed
roharon <roharon@studentpartner.com>
2019-08-11 21:45:16 +0900

    feat: Shuttle BusCard Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import 'rsuite/dist/styles/rsuite.min.css';
-import {Loader, Grid, Row} from 'rsuite';
+import {Container, Header, Content, Footer, Loader, Grid, Row} from 'rsuite';
+
+import CustomHeader from './header';
 import BusCard from './component/Shuttle';
 import './App.css';
+import { formatTestResults } from '@jest/test-result';
 
 class App extends Component {
 
@@ -48,14 +51,23 @@ class App extends Component {
     const buses = this.state.buses
 
     return(
-      <div>
+      <Container>
+        <Header>
+          <CustomHeader/>
+        </Header>
+
+        <Content>
         <Grid fluid style={{alignContent:'center'}}>
           <Row className="show-grid">
           {buses ? <BusCard data={buses}/> :  <Loader size="lg" content="으쌰으쌰 불러오고 있어요" /> }
-
           </Row>
         </Grid>
-      </div>
+        </Content>
+        
+        <Footer>
+
+        </Footer>
+      </Container>
     )
   }
 }
--- src/component/Shuttle.js
+++ src/component/Shuttle.js
@@ -1,30 +1,49 @@
 import React from 'react';
-import {Timeline, Popover, Col} from 'rsuite';
+import {Col, Icon, Timeline} from 'rsuite';
 //import {BrowserView, MobileView} from 'react-device-detect';
 import '../App.css'
+import './Shuttle.css'
 import { padding } from 'polished';
 
 const BusCard = ({data, key}) => {
     console.log(data)
     const bus = data.map((bus_arr) => {
+      
       return (
 
-        <Col xs={6} md={12} sm={24} xs={12} style={{height:350, width:180}}>
-        <Popover key={bus_arr.bus_number}title={bus_arr.bus_number} visible style={{width:180}}>
-        <Timeline>
-                  <Timeline.Item>외대사거리</Timeline.Item>
-                  <Timeline.Item>모현지석묘</Timeline.Item>
-                  <Timeline.Item>외대기숙사</Timeline.Item>
-                  <Timeline.Item>도서관</Timeline.Item>
-                  <Timeline.Item>학생회관</Timeline.Item>
-                  <Timeline.Item>어문관</Timeline.Item>
-                  <Timeline.Item>경상관</Timeline.Item>
-                </Timeline>
-        </Popover>
+        <Col xs={12} sm={12} md={24} lg={24} className="BusFull">
+        <Timeline className="BusCard">
+        <p>제목</p>
+        <Timeline.Item dot={<Icon icon="bus"/>} className="Bus__Column">
+            <p>인문경상관</p>
+          </Timeline.Item>
+          <Timeline.Item className="Bus__Column">
+            <p>어문학관</p>
+          </Timeline.Item>
+          <Timeline.Item className="Bus__Column">
+            <p>학생회관</p>
+          </Timeline.Item>
+          <Timeline.Item className="Bus__Column">
+            <p>공학관</p>
+          </Timeline.Item>
+          <Timeline.Item className="Bus__Column">
+            <p>백년관</p>
+          </Timeline.Item>
+          <Timeline.Item className="Bus__Column">
+            <p>정심대도</p>
+          </Timeline.Item>
+          <Timeline.Item className="Bus__Column">
+            <p>지석묘</p>
+          </Timeline.Item>
+          <Timeline.Item className="Bus__Column">
+            <p>모현사거리</p>
+          </Timeline.Item>
+        </Timeline>
+        <padding></padding>
         </Col>
                   
       )
-    })
+    });
   
   return bus
 }
